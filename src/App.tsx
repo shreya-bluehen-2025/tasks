a8951604ad2182a27e4c91729542702505ef1925
Shreya <shrey@udel.edu>
2023-02-15 15:50:26 -0500

    Final Final

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import elephant from "./images/elephant.jpg";
 
 export function App(): JSX.Element {
     return (
@@ -10,7 +9,7 @@ export function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript: Shreya Pamulapati
             </header>
             <h2> Hello World </h2>;
-            <img src={elephant} alt="My Favorite Animal" />
+            <img src="../images/elephant.jpg" alt="My Favorite Animal" />
             <ul>
                 <li>Elephants</li>
                 <li>Food</li>
