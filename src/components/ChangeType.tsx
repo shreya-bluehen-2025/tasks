71a1567ecf0cc224d0a662060c33d721c8b2cefe
Shreya <shrey@udel.edu>
2023-03-14 16:53:56 -0400

    made progress on task 8

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -2,6 +2,23 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
-export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+export function changeType(): JSX.Element {
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    function flip(): void {
+        if (type === "short_answer_question") {
+            setType("multiple_choice_question");
+        } else {
+            setType("short_answer_question");
+        }
+    }
+    return (
+        <div>
+            <Button onClick={flip}>Change Type</Button>
+            {type === "short_answer_question" ? (
+                <div>Short Answer</div>
+            ) : (
+                <div>Multiple Choice</div>
+            )}
+        </div>
+    );
 }
--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,38 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday = "🎃" | "🎂" | "🎄" | "💖" | "🎆";
+const alphabeticalOrder: Record<Holiday, Holiday> = {
+    "🎂": "🎄",
+    "🎄": "🎆",
+    "🎆": "🎃",
+    "🎃": "💖",
+    "💖": "🎂"
+};
+
+const chronologicalOrder: Record<Holiday, Holiday> = {
+    "💖": "🎂",
+    "🎂": "🎆",
+    "🎆": "🎃",
+    "🎃": "🎄",
+    "🎄": "💖"
+};
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("💖");
+    function changeLetter(): void {
+        const newHoliday = alphabeticalOrder[holiday];
+        setHoliday(newHoliday);
+    }
+    function changeYear(): void {
+        const newHoliday = chronologicalOrder[holiday];
+        setHoliday(newHoliday);
+    }
+    return (
+        <div>
+            <span>Holiday: {holiday}</span>
+            <Button onClick={changeLetter}>Move Up A Letter</Button>
+            <Button onClick={changeYear}>Move Up Year</Button>
+        </div>
+    );
 }
