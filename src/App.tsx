f3d6d18c33fb975bfc3da1adcbd884bd9791b1b6
Shreya <shrey@udel.edu>
2023-03-20 09:32:13 -0400

    final app

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { ChangeColor } from "./form-components/ChangeColor";
 import { CheckAnswer } from "./form-components/CheckAnswer";
 import { EditMode } from "./form-components/EditMode";
 import { GiveAttempts } from "./form-components/GiveAttempts";
+import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 
 function App(): JSX.Element {
     return (
