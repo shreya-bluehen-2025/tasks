befa6edbfc4b15c1e40df7daaf28d97882fbcaa9
Shreya <shrey@udel.edu>
2023-03-20 09:21:59 -0400

    done give attempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsRequested, setAttemptsRequested] = useState<number>(0);
+    function updateRequested(event: React.ChangeEvent<HTMLInputElement>) {
+        setAttemptsRequested(parseInt(event.target.value) || 0);
+    }
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>Attempts: {attemptsLeft}</div>
+            <Form.Group controlId="formRequestedAttempts">
+                <Form.Label>Requested Attempts:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attemptsRequested}
+                    onChange={updateRequested}
+                />
+            </Form.Group>
+            <Button
+                onClick={() => setAttemptsLeft(attemptsLeft - 1)}
+                disabled={attemptsLeft === 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() =>
+                    setAttemptsLeft(attemptsLeft + attemptsRequested)
+                }
+            >
+                gain
+            </Button>
         </div>
     );
 }
