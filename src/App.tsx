92f82c82b36a0f810e8e5439a02743f0b8207a62
Shreya <shrey@udel.edu>
2023-03-20 09:01:08 -0400

    changed app

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
+import { ChangeColor } from "./form-components/ChangeColor";
 import { CheckAnswer } from "./form-components/CheckAnswer";
 
 function App(): JSX.Element {
